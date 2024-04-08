f0df8fcd3d8b3029ac0a11514204c215c2cd0254
raku <lumixraku@gmail.com>
2024-04-08 22:06:20 +0800

    fix: drawText offset

--- src/demos/font-path.ts
+++ src/demos/font-path.ts
@@ -38,8 +38,8 @@ const runCanvaskitExample = async () => {
 
     //#endregion
 
-
-    const font = new CanvasKit.Font(typeface, 18);
+    // 最小字体 9 号 小于9 提示 zero-length path in getPosTa
+    const font = new CanvasKit.Font(typeface, 19);
     const fontPaint = new CanvasKit.Paint();
     fontPaint.setStyle(CanvasKit.PaintStyle.Fill);
     fontPaint.setAntiAlias(true);
@@ -49,32 +49,35 @@ const runCanvaskitExample = async () => {
     const paint = new CanvasKit.Paint();
     paint.setStyle(CanvasKit.PaintStyle.Fill);
     paint.setAntiAlias(true);
-    paint.setColor(CanvasKit.Color(1, 1, 1, 255));
-    
+    // paint.setColor(CanvasKit.Color(1, 1, 1, 255));
+    paint.setColor(CanvasKit.Color4f(0.5, 0.5, 1.0, 0.5)); // 浅蓝色，半透明
 
-    const circlePath = new CanvasKit.Path(); 
-    circlePath.addCircle(100, 100, 80);
+
+
+    const path = new CanvasKit.Path();
+    path.addCircle(100, 100, 80);
 
     const rectPath = new CanvasKit.Path();
-    rectPath.addRect(CanvasKit.LTRBRect(350, 0, 450, 100));   
-    circlePath.op(rectPath, PathKit.PathOp.UNION);
+    rectPath.addRect(CanvasKit.LTRBRect(350, 0, 450, 100));
+    path.op(rectPath, PathKit.PathOp.UNION);
 
     const trianglePath = new CanvasKit.Path();
     trianglePath.moveTo(650, 0);
     trianglePath.lineTo(650, 100);
-    trianglePath.lineTo(550, 100);
+    // 注意时针顺序  如果是顺时针, 文字将在 shape 内部
+    trianglePath.lineTo(550, 100); 
     trianglePath.close();
-    circlePath.op(trianglePath, PathKit.PathOp.UNION);
+    path.op(trianglePath, PathKit.PathOp.UNION);
     // Not working
     // const path = PathKit.MakeFromOp(circlePath, trianglePath, PathKit.PathOp.UNION);
     // console.log('path', path.toSVGString())
-    
+    skCanvas.drawPath(path, paint);
 
     // Create a TextBlob
     const elem = document.querySelector('#editor') as HTMLElement;
     const text = elem?.innerText;
-    const textBlob = CanvasKit.TextBlob.MakeOnPath(text, circlePath, font, 0);
-    skCanvas.drawTextBlob(textBlob, 20, 20, paint);
+    const textBlob = CanvasKit.TextBlob.MakeOnPath(text, path, font, 0);
+    skCanvas.drawTextBlob(textBlob, 0, 0, paint);
 
     surface.flush();
 };
